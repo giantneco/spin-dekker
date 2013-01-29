09d5e97b780fa0718c066d1c0213149f984ba319
giantneco <flszuk@gmail.com>
2013-01-30 00:24:51 +0900

    fixed claim.sh

--- claim.sh
+++ claim.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -f *rule
 spin -f '!(([] !( insideCriticalSection1 && insideCriticalSection2)))' >> exclusive.rule
 spin -f '!( [] (( outsideCriticalSection2 && X ! insideCriticalSection2) -> (X (! isLocked1))))' >> cooperation.rule
 spin -f '!(([] <> insideCriticalSection1))' >> faireness.rule
